93d1b7e202ecb7e606f2024e946ea4cf6df6e379
akihiyo <afukuma@icloud.com>
2015-06-18 10:54:20 +0900

    テスト

--- AWSFormSample/frmMain.Designer.vb
+++ AWSFormSample/frmMain.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmMain
     Inherits System.Windows.Forms.Form
 
     'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
@@ -24,15 +24,14 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.SuspendLayout()
         '
-        'Form1
+        'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(519, 456)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(526, 314)
+        Me.Name = "frmMain"
+        Me.Text = "frmMain"
         Me.ResumeLayout(False)
 
     End Sub
-
 End Class
---
+++ AWSFormSample/frmMain.vb
@@ -0,0 +1,44 @@
+﻿Public Class frmMain
+    '1:フォームロード時に、アカウントファイルを読み込む
+    '処理：CSVファイルを読み込み、パラメータを取得する
+
+    Private _AWSID As String = ""
+    Private _AWSKEY As String = ""
+
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim wTitle As String = ""
+
+        'Open File Dialog --- Select Certifaction file.(download from aws console)
+        Dim Ofd As New OpenFileDialog
+        Ofd.Filter = "csv File(*.csv)|*.csv|ALL File(*.*)|*.*"
+        Ofd.FilterIndex = 2
+        Ofd.Title = "Select AWS CertFile"
+        Ofd.RestoreDirectory = True
+        If Ofd.ShowDialog() = DialogResult.OK Then
+            Dim Sr As New System.IO.StreamReader(Ofd.FileName) '  System.Text.Encoding.GetEncoding("shift_jis"))
+            Dim wLine As String = "" 'Hold the Last line
+            While Sr.Peek() > -1
+                wLine = Sr.ReadLine()
+            End While
+            Sr.Close()
+
+            'Seplate wLine
+            Dim wCell() As String = wLine.Split(",")
+            If wCell.Length = 3 Then
+                wTitle = wCell(0)
+                _AWSID = wCell(1)
+                _AWSKEY = wCell(2)
+            End If
+        End If
+
+        If wTitle.Length = 0 Then
+            MsgBox("You chose an illegal authentication file")
+            End
+        Else
+            Me.Text = "AWSFormSample -- " + wTitle
+        End If
+
+    End Sub
+
+End Class
